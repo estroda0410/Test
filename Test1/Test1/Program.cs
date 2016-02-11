14ae6464b0ab9ac8b125bec47aeb6a902d04d58d
Erin Stroda <Erin Stroda>
2016-02-10 20:02:41 -0800

    Testing grounds for CodeWars

--- Test1/Test1/Program.cs
+++ Test1/Test1/Program.cs
@@ -10,6 +10,22 @@ namespace Test1
     {
         static void Main(string[] args)
         {
+            string go = "yes";
+            do
+            {
+                Console.WriteLine("Please enter your string...");
+
+                string s = Console.ReadLine();
+
+                char[] arr = s.ToCharArray(0, s.Length);
+
+                Console.Write(arr);
+                Console.WriteLine();
+                Console.WriteLine("Enter 'yes' to continue...");
+                
+                go = Console.ReadLine();
+            }
+            while (go == "yes");
         }
     }
 }
